fc003df307e30385e269ed7e6c86327c1513148e
Glaucia Melo <glauciamelo@gmail.com>
2017-06-07 12:07:54 -0300

    First Commit

---
+++ src/Suggestion.java
@@ -0,0 +1,30 @@
+
+public class Suggestion {
+
+	private int id;
+	private String titulo;	
+	private String postId;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getPostId() {
+		return postId;
+	}
+	public void setPostId(String postId) {
+		this.postId = postId;
+	}
+	
+	
+	
+}
